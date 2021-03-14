071b0fcca8beeb8e85906095ff2ef3a603808566
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2021-03-14 16:51:08 +0000

    Add files via upload
    auto installer

---
+++ install.bash
@@ -0,0 +1,8 @@
+echo Thanks for downlaoding CVXR
+sudo apt install pip
+pip install psutil
+echo =====================END OF INSTALL==============================
+echo  
+echo  
+echo Great now CVXR is now setup you can now run
+echo python3 CVXR-CPU.py or CVXR-RAM.py or CVXR-DISK.py
